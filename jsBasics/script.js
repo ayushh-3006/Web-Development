ec9e01f93f21e128ce52e933d5f5f6913449c9ae
ayushh-3006 <ayushkumarsingh3006@gmail.com>
2026-04-21 22:55:22 +0530

    basic javascript

--- jsBasics/script.js
+++ jsBasics/script.js
@@ -9,6 +9,18 @@ console.log(a===b);
 console.log(typeof a);
 console.log(typeof b);
 
+let i=1;
+
+while(i<20)
+{
+    if(i%2==0)
+    {
+        console.log(i);
+        
+    }
+    i++;
+}
+
 
 
 
