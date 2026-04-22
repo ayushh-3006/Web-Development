8afdb225a41eab8d332820831bbf0cc84e872c21
ayushh-3006 <ayushkumarsingh3006@gmail.com>
2026-04-22 21:43:33 +0530

    loops in js

---
+++ jsBasics/script1.js
@@ -0,0 +1,217 @@
+// question 1
+
+for(let i = 1; i<=10; i++)
+{
+   console.log(i);
+   console.log("hello");
+}
+
+
+
+// question3
+
+for(let i=1; i<=21; i++){
+    if(i%2==0){
+
+         console.log(i);
+
+    }
+     
+}
+
+// qusetion4
+
+let i = 1;
+while(i<16){
+    if(i%2==1){
+        console.log(i);
+    }
+
+    i++;
+
+}
+
+// question5
+
+for(let i=1; i<=10; i++){
+    console.log(`5*${i} = ${5*i}`);
+}
+
+// question6
+let sum = 0;
+for(let i =1; i<=100; i++){
+sum = sum+i;
+console.log(sum);
+}
+
+// question7
+
+for(let i=1; i<=50; i++){
+    if(i%3==0){
+        console.log(i);
+    }
+}
+
+// question8
+
+let val = prompt("give a number");
+for(let i=1; i<=val; i++){
+    if(i%2==0){
+        console.log(`${i} is even`)
+    }
+    else{
+        console.log(`${i} is odd`)
+    }
+}
+
+
+// question9
+
+for(let i= 1; i<=100; i++){
+    if(i%3==0 && i%5==0)
+    {
+        console.log(i);
+    }
+}
+
+// question1 using break
+
+for(let i= 1; i<=100; i++){
+    console.log(i);
+    if(i%7==0){
+        break;
+    }
+
+}
+
+// question2 using continue
+
+for(let i=1; i<21; i++){
+    
+    if(i%3==0)
+        continue;
+         console.log(i);
+    
+   
+}
+
+// question3 use continue,break
+
+let count = 0;
+for(let i=1; i<101; i++){
+    if(i%2===1){
+        count++;
+        console.log(i);
+    }
+
+    if(count===5)
+        break;
+}// question 1
+
+for(let i = 1; i<=10; i++)
+{
+   console.log(i);
+   console.log("hello");
+}
+
+
+
+// question3
+
+for(let i=1; i<=21; i++){
+    if(i%2==0){
+
+         console.log(i);
+
+    }
+     
+}
+
+// qusetion4
+
+let i = 1;
+while(i<16){
+    if(i%2==1){
+        console.log(i);
+    }
+
+    i++;
+
+}
+
+// question5
+
+for(let i=1; i<=10; i++){
+    console.log(`5*${i} = ${5*i}`);
+}
+
+// question6
+let sum = 0;
+for(let i =1; i<=100; i++){
+sum = sum+i;
+console.log(sum);
+}
+
+// question7
+
+for(let i=1; i<=50; i++){
+    if(i%3==0){
+        console.log(i);
+    }
+}
+
+// question8
+
+let val = prompt("give a number");
+for(let i=1; i<=val; i++){
+    if(i%2==0){
+        console.log(`${i} is even`)
+    }
+    else{
+        console.log(`${i} is odd`)
+    }
+}
+
+
+// question9
+
+for(let i= 1; i<=100; i++){
+    if(i%3==0 && i%5==0)
+    {
+        console.log(i);
+    }
+}
+
+// question1 using break
+
+for(let i= 1; i<=100; i++){
+    console.log(i);
+    if(i%7==0){
+        break;
+    }
+
+}
+
+// question2 using continue
+
+for(let i=1; i<21; i++){
+    
+    if(i%3==0)
+        continue;
+         console.log(i);
+    
+   
+}
+
+// question3 use continue,break
+
+let count = 0;
+for(let i=1; i<101; i++){
+    if(i%2===1){
+        count++;
+        console.log(i);
+    }
+
+    if(count===5)
+        break;
+}
