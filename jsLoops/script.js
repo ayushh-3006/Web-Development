2782b0ca52b651b123f41b899c32c303ef489f56
ayushh-3006 <ayushkumarsingh3006@gmail.com>
2026-04-29 21:43:37 +0530

    loops in javascript

---
+++ jsLoops/script.js
@@ -0,0 +1,73 @@
+
+// for(let i = 1; i<=10; i++)
+// {
+//    console.log(i);
+//    console.log("hello");
+// }
+
+
+
+console.log("2 ka table");
+
+
+for(let i=1; i<=21; i++){
+    if(i%2==0){
+
+         console.log(i);
+
+    }
+     
+}
+
+let j=1;
+while( j<=21)
+{
+    if(j%2==0)
+    {
+      console.log(j);
+    }
+    j++; 
+}
+
+console.log("3 ka table");
+
+
+
+for(let i=3; i<=31; i++){
+    if(i%3==0){
+
+         console.log(i);
+
+    }
+     
+}
+
+console.log("print all elements in array");
+
+
+let a = [24,25,26,35,6,95,85,64,100,null]
+
+
+
+
+for(let i=0;i<10;i++)
+{
+    console.log(a[i]);
+    
+}
+
+
+let i=0;
+while(i<10)
+{
+console.log(a[i]);
+i++;
+
+}
+
+
+a.forEach(element => console.log(element));
+
+
+
+
