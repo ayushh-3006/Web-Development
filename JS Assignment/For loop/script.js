40e99ff0fd934091ef4492ac6e66daa0b03d5003
ayushh-3006 <ayushkumarsingh3006@gmail.com>
2026-04-30 18:29:40 +0530

    for loop and while loop assignment in javascript

---
+++ JS Assignment/For loop/script.js
@@ -0,0 +1,85 @@
+// 1.Print numbers from 1 to 5 using a for loop.
+
+for(let i=1; i<=5;i++)
+{
+    console.log(i);
+    
+}
+
+// 2.Print numbers from 1 to 10 using a for loop.
+
+for(let i=1; i<=10;i++)
+{
+    console.log(i);
+    
+}
+
+// 3.Print numbers from 10 to 1 (reverse counting). 
+
+
+for(let i=10; i>=1;i--)
+{
+    console.log(i);
+    
+}
+
+// 4.Print even numbers from 2 to 10. 
+
+
+for(let i=2; i<21;i++)
+{
+    if(i%2==0)
+{
+console.log(i);
+
+}
+}
+
+//Print odd numbers from 1 to 9. 
+for(let i = 1; i<10; i++){
+    if(i%2!==0){
+        console.log(i);
+        
+    }
+}
+
+//Print numbers from 1 to 20 with increment of 2
+
+for(let i = 1; i<20; i+=2){
+    
+    console.log(i);
+    
+}
+
+//Print numbers from 5 to 50 with increment of 5. 
+
+for(let i = 5; i<50; i+=5){
+    
+    console.log(i);
+    
+}
+
+
+// Print numbers from 1 to 10 that are divisible by 3. 
+for(let i = 1; i<=10; i++){
+    if(i%3==0){
+        console.log(i);
+        
+    }
+
+}
+
+for (let i = 1; i <= 10; i++) {
+  let square = i * i;
+  console.log('Square of ${i} is ${square}');
+}
+
+for(let m=1;m<11;m++)
+{
+    if(m%3==0)
+    {
+        console.log(m);
+        
+    }
+}
+
