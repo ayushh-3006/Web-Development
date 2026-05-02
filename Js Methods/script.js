1031cecae46834a60e56328b6d647a886ceaf376
ayushh-3006 <ayushkumarsingh3006@gmail.com>
2026-05-02 11:08:25 +0530

    javascript functions

---
+++ Js Methods/script.js
@@ -0,0 +1,15 @@
+let a = 20,
+  b = 30;
+
+function sum(x, y) {
+  let z = x + y;
+  console.log(z);
+}
+
+sum(a, b);
+
+function greetings() {
+  console.log("hello ");
+}
+
+greetings();
---
+++ Js Methods/script1.js
@@ -0,0 +1,22 @@
+const abc = function () {
+  console.log("hello world");
+};
+
+console.log(abc);
+
+abc(); // calling of a function known as callback function
+
+// this is called functtion expressions
+
+
+const arrowfun = () => {
+    console.log("i am arrow function");
+    
+};
+
+arrowfun();
+
+const addarrow = (a,b) => a+b;
+
+console.log(addarrow(5,6));
+
