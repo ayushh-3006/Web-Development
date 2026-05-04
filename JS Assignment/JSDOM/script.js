8f9143713c1015fac65df12c1da661d3d6bad2eb
ayushh-3006 <ayushkumarsingh3006@gmail.com>
2026-05-04 20:54:36 +0530

    DOM basics

---
+++ JS Assignment/JSDOM/script.js
@@ -0,0 +1,12 @@
+function submit() {
+  console.log("submit button clicked");
+
+  const FN = document.getElementById("fullname").value;
+
+  console.log(FN);
+
+  
+
+  //    document.getElementById("myData").innerText=FN;
+  //    document.getElementById("fullName").value= "";
+}
