ca6f8d35f4621e1299e0e35cadffdb18bd37de60
ayushh-3006 <ayushkumarsingh3006@gmail.com>
2026-05-07 15:36:21 +0530

    gettt element by id in dom js

--- JS Assignment/JSDOM/script.js
+++ JS Assignment/JSDOM/script.js
@@ -1,12 +1,35 @@
-function submit() {
-  console.log("submit button clicked");
+// function submit() {
+//   console.log("submit button clicked");
 
-  const FN = document.getElementById("fullname").value;
+//   const FN = document.getElementById("fullname").value;
 
-  console.log(FN);
+//   console.log(FN);
 
   
 
-  //    document.getElementById("myData").innerText=FN;
-  //    document.getElementById("fullName").value= "";
+//   //    document.getElementById("myData").innerText=FN;
+//   //    document.getElementById("fullName").value= "";
+// }
+
+function Submit() {
+  const fn = document.getElementById("fullName").value;
+  console.log("Submit Button Clicked");
+  console.log(fn);
+  document.getElementById("myData").innerText = fn;
+  document.getElementById("fullName").value = "";
+
+  document.getElementById("hideButton").classList.remove("divHide");
+  document.getElementById("dataCard").classList.remove("divHide");
+}
+
+function Hide() {
+  document.getElementById("dataCard").classList.add("divHide");
+  document.getElementById("hideButton").classList.add("divHide");
+}
+function showAlert() {
+  document.getElementById("customAlert").style.display = "block";
+}
+
+function closeAlert() {
+  document.getElementById("customAlert").style.display = "none";
 }
