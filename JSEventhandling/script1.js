6b8cc71de9438d296e9a06b5aa5e34d5db486d30
ayushh-3006 <ayushkumarsingh3006@gmail.com>
2026-05-08 12:13:02 +0530

    eventtt handling applicaion in js

---
+++ JSEventhandling/script1.js
@@ -0,0 +1,32 @@
+
+const contentDiv = document.getElementById('box1');
+const heading = document.getElementById('heading');
+const paragraph = document.getElementById('para');
+
+// const inputBg = document.getElementById('input-bg');
+// const inputHeading = document.getElementById('input-heading');
+// const inputPara = document.getElementById('input-para');
+const btnReset = document.getElementById('btn-reset');
+
+
+function changeBg() {
+    contentDiv.style.backgroundColor = document.getElementById('input-bg').value;
+}
+
+function changeHeadingColor() {
+    heading.style.color = document.getElementById('input-heading').value;
+}
+
+function changeParaColor() {
+    paragraph.style.color = document.getElementById('input-para').value;
+}
+
+function resetPage() {
+    window.location.reload();
+}
+
+
+document.getElementById("input-bg").addEventListener("input", changeBg);
+document.getElementById('input-heading').addEventListener("input", changeHeadingColor);
+document.getElementById('input-para').addEventListener("input", changeParaColor);
+btnReset.addEventListener("click", resetPage);
