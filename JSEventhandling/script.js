c32d8262d72049aac7f3dfb45a09c35c5b99c787
ayushh-3006 <ayushkumarsingh3006@gmail.com>
2026-05-08 12:00:52 +0530

    event handling in javascript

---
+++ JSEventhandling/script.js
@@ -0,0 +1,27 @@
+// function bulb4on() {
+//   document.getElementById("bulb1").style.backgroundColor = "yellow";
+// }
+
+// function bulb5on() {
+//   document.getElementById("bulb1").style.backgroundColor = "pink";
+// }
+
+// function bulb2on() {
+//   document.getElementById("bulb1").style.backgroundColor = "red";
+// }
+
+// function bulb3on() {
+//   document.getElementById("bulb1").style.backgroundColor = "Green";
+// }
+
+function bulb1on() {
+  document.getElementById("bulb1").style.backgroundColor = "blue";
+}
+
+function bulb1off() {
+  document.getElementById("bulb1").style.backgroundColor = "white";
+}
+
+document.getElementById(bulbred).addEventListener("click", "bulbred");
+document.getElementById(bulb).addEventListener("click", "bulbred");
+document.getElementById(bulbred).addEventListener("click", "bulbred");
