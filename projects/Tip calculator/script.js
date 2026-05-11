55e859aa48ae9e2b2554972216c27f3c56af00de
ayushh-3006 <ayushkumarsingh3006@gmail.com>
2026-05-11 13:28:27 +0530

    2nd method in ttip calculator

--- projects/Tip calculator/script.js
+++ projects/Tip calculator/script.js
@@ -13,3 +13,20 @@ function calculateTip() {
 
     document.getElementById("tip").innerHTML =`Tip Amount<br><strong>${tipEach.toFixed(2)} ₹ each</strong>`;       
 }
+
+
+
+// document.querySelector("form").addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   const bill = document.getElementById("billAmount").value;
+//   const ser = document.getElementById("serviceLevel").value;
+//   const pc = document.getElementById("peoplecount").value;
+
+//   const tipPerPerson = (bill * ser) / pc;
+
+//   //   console.log(tipPerPerson);
+//   document.getElementById("billAmount").value = "";
+//   document.getElementById("serviceLevel").value = "";
+//   document.getElementById("peoplecount").value = "";
+//   document.getElementById("tipInfo").innerText = tipPerPerson;
+// });
