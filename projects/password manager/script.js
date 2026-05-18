729e7d6d55f9d22d04e759756288c93d87a6b244
ayushh-3006 <ayushkumarsingh3006@gmail.com>
2026-05-18 13:57:08 +0530

    completed the password manager

--- projects/password manager/script.js
+++ projects/password manager/script.js
@@ -54,23 +54,65 @@
 //   localStorage.removeItem("PasswordManager");
 // }
 
-document.getElementById("passform").addEventListener("submit", (event) => {
+// document.getElementById("passform").addEventListener("submit", (event) => {
+//   event.preventDefault();
+
+//   const websiteName = document.getElementById("siteName").value.trim();
+//   const userName = document.getElementById("userName").value.trim();
+//   const Password = document.getElementById("password").value.trim();
+
+//   const dataPacket = {
+//     webSite: websiteName,
+//     userName: userName,
+//     password: Password,
+//   };
+
+//   function savetolocalstorage(dataPacket) {
+//     const olddata = JSON.parse(localStorage.getItem("passwords"));
+//   }
+
+
+
+
+
+
+// });
+
+
+
+document.getElementById("passFrom").addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const websiteName = document.getElementById("siteName").value.trim();
+  const webSiteName = document.getElementById("siteName").value.trim();
   const userName = document.getElementById("userName").value.trim();
-  const Password = document.getElementById("password").value.trim();
+  const password = document.getElementById("password").value;
 
-  const dataPacket = {
-    webSite: websiteName,
-    userName: userName,
-    password: Password,
+  const packet = {
+    WebsiteName: webSiteName,
+    UserName: userName,
+    Password: password,
   };
 
-  function savetolocalstorage(dataPacket) {
-    const olddata = JSON.parse(localStorage.getItem("passwords"));
-  }
+  saveToLocalStorage(packet);
 
-
-
+  document.getElementById("siteName").value = "";
+  document.getElementById("userName").value = "";
+  document.getElementById("password").value = "";
 });
+
+document.getElementById("passFrom").addEventListener("reset", (event) => {
+  event.preventDefault();
+  document.getElementById("siteName").value = "";
+  document.getElementById("userName").value = "";
+  document.getElementById("password").value = "";
+});
+
+function saveToLocalStorage(packet) {
+  const oldData = JSON.parse(localStorage.getItem("Passwords")) || [];
+
+  oldData.push(packet);
+
+  const packetInString = JSON.stringify(oldData);
+
+  localStorage.setItem("Passwords", packetInString);
+}
