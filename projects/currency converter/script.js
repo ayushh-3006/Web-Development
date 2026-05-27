1ec1af6dff6cbf056c9cc2e9d03879b496297b5c
ayushh-3006 <ayushkumarsingh3006@gmail.com>
2026-05-27 21:55:22 +0530

    currency converter

--- projects/currency converter/script.js
+++ projects/currency converter/script.js
@@ -100,7 +100,7 @@ async function convertCurrency() {
 
   try {
     const response = await fetch(
-      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${fromCurrency}.json`,
+   //   `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${fromCurrency}.json`,
     );
 
     const data = await response.json();
