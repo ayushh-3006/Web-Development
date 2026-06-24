83abddcd602a301fccc56dd39562e653035fe644
ayushh-3006 <ayushkumarsingh3006@gmail.com>
2026-06-24 21:11:37 +0530

    chore: remove commented-out code from dbConnection configuration

--- Backend/FirstBackend/src/config/dbConnection.config.js
+++ Backend/FirstBackend/src/config/dbConnection.config.js
@@ -12,3 +12,18 @@ const connectDB = async () => {
   }
 };
 export default connectDB;
+
+// import mongoose from "mongoose";
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_DB_URI);
+//     console.log("Mongo DB Connect Successfully");
+//     console.log("DB Host:", conn.connection.host);
+
+//     console.log("DB Name:", conn.connection.name);
+//   } catch (error) {
+//     console.log(error.message);
+//     process.exit(1);
+//   }
+// };
+// export default connectDB;
